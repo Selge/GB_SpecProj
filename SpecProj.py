db2dfae3c22945249aded3c5fc8e3fe85b93c708
Sergei Shcherbak <sshch1987@gmail.com>
2022-11-28 12:52:28 +0200

    The same task, but in Python 3.

---
+++ SpecProj.py
@@ -0,0 +1,24 @@
+# The same task, but in Python 3.
+
+def fill_array():
+    array = []
+    print('Please, fill in a desired array: ')
+    while True:
+        a = str(input("Enter an element (type 'Space' to break'): "))
+        if a == ' ':
+            break
+        array.append(a)
+    return array
+
+
+def three_or_less():
+    array = fill_array()
+    triple_array = []
+    for i in array:
+        if len(i) <= 3:
+            triple_array.append(i)
+    return triple_array
+
+
+if __name__ == '__main__':
+    three_or_less() 
